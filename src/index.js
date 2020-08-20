d2a7ccc0d3356df35e278938f215be89bcaa905a
memellaguno <memellaguno@gmail.com>
2020-08-20 10:42:41 -0500

    Task lisk generation working

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-import todoModule from "./todoModule.js";
+import View from "./view.js";
+import Model from "./model.js"
+import Controller from "./controller.js"
+
+//Instatiate
+const model = Model();
+const controller = Controller(model);
+const view = View(controller);
+
+
+
+
+//Materialize
 M.AutoInit();
-todoModule();
 
